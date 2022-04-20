a54a712259cf057be0552e3ef23cf41c9108b9bb
CLEMS3 <clement.chapard@insa-lyon.fr>
2022-04-20 18:37:16 +0200

    debut de la fonction juger pour les pions

--- Jeu de dame.py
+++ Jeu de dame.py
@@ -48,13 +48,43 @@ class Game():
     #x,y: position de la pion/dame
     #nx,ny: nouvelle potition de la pion
     #return: -1: pas valide; 0: pion avancer ; 1: pion manger; 2:dame déplacer 3: dame manger
-    def juger(self,x,y,nx,n_y)->int:
-        pass
+    def juger(self,x,y,n_x,n_y)->int:
+        """il y a beaucoup de if les uns dans les autres, on pourra simplifier ça une fois que ça marche"""
+        #test si la pièce est un pion ou une dame
+        pion=self.damier[x][y]
+        print(pion.couleur)
+        if not pion.Dame:
+            # test si la case d'arrivé est libre
+            if not self.damier[n_x][n_y]:
+                #test si il est possible d'y aller en avançant
+                if abs(n_y-y) == 1 and 0<=n_y<10:
+                    if pion.couleur:
+                        print(0 if n_x == x-1 and x>0 else "a")
+                    else:
+                        print(0 if n_x == x + 1 and x < 10 else -1)
+                else: print(-1)
+                #test si il est possible d'y aller en mangeant
+                if n_y == y-2 and 0<=n_y<10: #à gauche
+                    if pion.couleur:
+                        print(1 if self.damier[x-1][y-1] else -1)
+                    else:
+                        print(1 if self.damier[x + 1][y - 1] else -1)
+                elif n_y == y+2 and 0<=n_y<10: #à droite
+                    if pion.couleur:
+                        print(1 if self.damier[x-1][y+1] else -1)
+                    else:
+                        print(1 if self.damier[x + 1][y+1] else -1)
+                else : print("b") #attention, ne pas verifier si l'on peut y aller en mangeant si on peut y aller en se déplaçant
 
+
+            else:
+                print(-1)
 
 game=Game()
 game.affichage()
-game.avance(3,2,'l')
+game.juger(3,4,4,3)
+game.avance(3,4,'l')
+
 game.affichage()
 
 #test
