3c3429db1fa9bf23d5ffd931f8b3dab89acfbbc8
Sun Jixiang <jixiang.sun@insa-lyon.fr>
2022-04-28 13:39:15 +0200

    reformuler la fonction juge

--- Jeu de dame.py
+++ Jeu de dame.py
@@ -9,7 +9,7 @@ class Game():
     damier=[[False for j in range(10)]for i in range(10)]   #False: Pas de pion sur la case
     tourne = False  #tourne des joueurs. Noir:True Blanc=False
     def __init__(self) -> None:
-        
+
         #Initialization de damier
         for i in range(4):
             for j in range(0+(not i%2),10,2):
@@ -47,88 +47,77 @@ class Game():
             print('Pas de pion ici')
     
     #x,y: position de la pion/dame
-    #nx,ny: nouvelle potition de la pion
-    #return: -1: pas valide; 0: pion avancer ; 1: pion manger; 2:dame déplacer 3: dame manger
-    def juger(self,x,y,n_x,n_y)->int:
+    #nx,ny: nouvelle potition de la pion/dame
+    #return: -1: pas valide; 0: déplacer ; 1: manger
+    def juger(self,x:int,y:int,n_x:int,n_y:int)->int:
         """il y a beaucoup de if les uns dans les autres, on pourra simplifier ça une fois que ça marche"""
-        #test si la pièce est un pion ou une dame
         pion=self.damier[x][y]
-        if not pion.Dame:
-            # test si la case d'arrivé est libre
-            if not self.damier[n_x][n_y]:
-                av = False
-                #test si il est possible d'y aller en avançant
-                if abs(n_y-y) == 1 and 0<=n_y<10:
-                    av=True
-                    if not pion.couleur:
-                        print(0 if n_x == x-1 and x>0 else -1)
-                    else:
-                        print(0 if n_x == x + 1 and x < 10 else -1)
-                else: print(-1)
-                #test si il est possible d'y aller en mangeant
-                #/!\ prise obligatoire, verifier si il y a une prise possible avant de regarder si on peut avancer
-                if not av:
-                    if n_y == y-2 and 0<=n_y<10: #à gauche
-                        if not pion.couleur:
-                            print(1 if self.damier[x-1][y-1] else -1)
-                        else:
-                            print(1 if self.damier[x + 1][y - 1] else -1)
-                    elif n_y == y+2 and 0<=n_y<10: #à droite
-                        if not pion.couleur:
-                            print(1 if self.damier[x-1][y+1] else -1)
-                        else:
-                            print(1 if self.damier[x + 1][y+1] else -1)
-                    else : print("b") #attention, ne pas verifier si l'on peut y aller en mangeant si on peut y aller en se déplaçant
 
+        if not pion:
+            print('Pas de pion ici')
+            return -1
+        if pion.couleur !=self.tourne:
+            print("C'est pas votre pion!")
+            return -1
+        if not (0<=n_x<10 and 0<=n_y<10):
+            print("En dehors de la damier!")
+            return -1
+        if self.damier[n_x][n_y]:# test si la case d'arrivé est occupée
+            print("déjà un pion/une dame ici")
+            return -1
 
+        diff_x = n_x - x
+        diff_y = n_y - y
+        if abs(diff_y) != abs(diff_x):
+            print("Diagonale svp!")
+            return -1
 
-            else:
-                print(-1)
-        #on traite le cas des dames
-        else:
-            # test si la case d'arrivé est libre
-            if not self.damier[n_x][n_y]:
-                av = False
-                # test si il est possible d'y aller en avançant
-                diff_x = n_x - x
-                diff_y = n_y - y
-                sign = lambda x: True if x>0 else False
-                if abs(diff_y) == abs(diff_x) and 0<= n_x <10 and 0<= n_y <10: #test si on est sur la diagonale
-                    #test si toutes les cases sur la diagonales sont libre (oui -> avancer, oui sauf l'avant dernière -> manger, non -> pas possible)
-                    free_diagonal = True
-                    xd, yd = x, y
-                    i = 0
-                    while i < abs(diff_x) and free_diagonal:
-                        i+=1
-                        xd = xd + 1 if diff_x>0 else xd-1
-                        #print(xd)
-                        yd = yd + 1 if diff_y > 0 else yd - 1
-                        #print(yd)
-                        #print(i)
-                        if self.damier[xd][yd]:
-                            free_diagonal = False
-                            #print(i)
-                    avx = n_x -1 if diff_x>0 else n_x +1
-                    avy = n_y -1 if diff_y>0 else n_y +1
-                    if free_diagonal:
-                        print(2)
-                    elif self.damier[avx][avy]:
-                        print(3)
-                    else:
-                        print(-1)
+        
+        if not pion.Dame: #on traite le cas des pions
+            if (pion.couleur and diff_x<0) or (not pion.couleur and diff_x>0):
+                print("un pion ne peut pas aller en arrière")
+                return -1
+            if abs(n_y-y) == 1: #test si il est possible d'y aller en avançant
+                return 0    #déplacer
+            if abs(n_y-y)==2:   #test si il est possible d'y aller en mangeant
+            #/!\ prise obligatoire, verifier si il y a une prise possible avant de regarder si on peut avancer
+                à_manger=self.damier[x+diff_x/2][y+diff_y/2]
+                if not à_manger or à_manger.couleur == self.tourne:
+                    print("on peut pas manger!")
+                    return -1
+                return 1 #manger
+            
+            #le cas dessous est donc quand abs(n_y-y)!=1, ni != 2
+            print("un pion ne peut pas aller si loin")
+            return -1
+        
+        else: #on traite le cas des dames
+            #test si toutes les cases sur la diagonales sont libre (oui -> avancer, oui sauf l'avant dernière -> manger, non -> pas possible)
+            free_diagonal = True #la diagonal entre x,y (non compris) et l'avant dernière(non compris)
+            xd, yd = x, y
+            i = 1
+            while i < abs(diff_x)-1 and free_diagonal:
+                xd = xd + 1 if diff_x>0 else xd-1
+                yd = yd + 1 if diff_y > 0 else yd - 1
+                if self.damier[xd][yd]:
+                    free_diagonal = False
+                i+=1
 
+            avx = n_x -1 if diff_x>0 else n_x +1
+            avy = n_y -1 if diff_y>0 else n_y +1
 
-                else:
-                    print(-1)
-            else:
-                print(-1)
-
+            if free_diagonal and not self.damier[avx][avy]:
+                return 0
+            if free_diagonal and self.damier[avx][avy].couleur!=self.tourne:
+                return 1
+            
+            #le cas dessous est donc soit diagonal n'est pas libre, soit on mange le faux pion
+            print("vous mangez trop!")
+            return -1
 
 game=Game()
+
 game.affichage()
 game.juger(3,4,4,3)
 game.avance(3,4,'l')
-
 game.affichage()
-
-#test
